277b5e75059f1601b0af87fd6f9d10bfdd57bd3e
piznel <piznel@hotmail.com>
2019-01-11 16:11:12 +0100

    second commit !

--- controller/alexaController.js
+++ controller/alexaController.js
@@ -1,17 +1,8 @@
-loadData = require('../lib/alexa.loadData.js');
 saveData = require('../lib/alexa.saveData.js');
 getDeviceTypes = require('../lib/alexa.getDeviceTypes.js')
 
 
 module.exports = {
-
-  loadData: function(req, res, next) {
-    loadData()
-      .then((result) => {
-        res.json(result)
-      })
-      .catch(next);
-  },
 
   saveData: function(req, res, next) {
     saveData(req.body)
--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 module.exports = function(sails) {
-    const alexa = require('./lib/alexa.js');
-    const alexaController = require('./controller/alexaController.js')
-  
-    gladys.on('ready', function() {
-      alexa();
-    });
-  
-    return {
-      routes: {
-        before: {
-          'get /alexa/dataload': (req, res, next) => sails.hooks.policies.middleware.checktoken(req, res, next),
-          'patch /alexa/datasave': (req, res, next) => sails.hooks.policies.middleware.checktoken(req, res, next),
-          'get /alexa/device': (req, res, next) => sails.hooks.policies.middleware.checktoken(req, res, next)
-  
-        },
-        after: {
-          'get /alexa/dataload': alexaController.loadData,
-          'patch /alexa/datasave': alexaController.saveData,
-          'get /alexa/device': alexaController.getDeviceTypes
-        }
+  const alexa = require('./lib/alexa.js');
+  const alexaController = require('./controller/alexaController.js')
+
+  gladys.on('ready', function() {
+    alexa();
+  });
+
+  return {
+    routes: {
+      before: {
+        'patch /alexa/datasave': (req, res, next) => sails.hooks.policies.middleware.checktoken(req, res, next),
+        'get /alexa/device': (req, res, next) => sails.hooks.policies.middleware.checktoken(req, res, next)
+
+      },
+      after: {
+        'patch /alexa/datasave': alexaController.saveData,
+        'get /alexa/device': alexaController.getDeviceTypes
       }
-    };
+    }
   };
+};
