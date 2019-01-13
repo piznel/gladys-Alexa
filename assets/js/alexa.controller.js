cf198cb942cfb55f5c2d49c18f4fc3cd7dbd1e46
piznel <piznel@hotmail.com>
2019-01-13 11:32:14 +0100

    Add translation

--- assets/js/alexa.controller.js
+++ assets/js/alexa.controller.js
@@ -1,51 +1,62 @@
 (function() {
-    'use strict';
-  
-    angular
-      .module('gladys')
-      .controller('alexaCtrl', alexaCtrl);
-  
-    alexaCtrl.$inject = ['alexaService', '$scope'];
-  
-    function alexaCtrl(alexaService, $scope) {
-      /* jshint validthis: true */
-      var vm = this;
-  
-      vm.rooms = [];
-      vm.remoteIsBusy = false;
-      vm.ready = false;
-      vm.devicetypes = [];
-      vm.alexaDevices = []
-  
-      vm.saveConfig = saveConfig;
-  
-      activate()
-  
-      function activate() {
-        vm.remoteIsBusy = true;
-        return alexaService.getDeviceTypes()
-          .then(function(data) {
-            vm.alexaDevices = data.data
-            vm.ready = true;
-            vm.remoteIsBusy = false;
-            return
-          })
-          .catch(function(err) {
-            console.log(err)
-          })
-      }
-  
-      function saveConfig() {
-        var alexaDevice = {};
-  
-        vm.alexaDevices.forEach(function (room) {
-            room.deviceTypes.forEach(function (deviceType) {
-                if(deviceType.friendlyName) {
-                  alexaDevice[deviceType.id] = {'alexa': deviceType.alexa, 'friendlyName': deviceType.friendlyName}
-                }
-            });
-        });
-        alexaService.saveConfig(alexaDevice)
-      }
+  'use strict';
+
+  angular
+    .module('gladys')
+    .controller('alexaCtrl', alexaCtrl);
+
+  alexaCtrl.$inject = ['alexaService', '$scope'];
+
+  function alexaCtrl(alexaService, $scope) {
+    /* jshint validthis: true */
+    var vm = this;
+
+    vm.rooms = [];
+    vm.remoteIsBusy = false;
+    vm.ready = false;
+    vm.devicetypes = [];
+    vm.alexaDevices = []
+
+    vm.saveConfig = saveConfig;
+
+    activate()
+
+    function activate() {
+      vm.remoteIsBusy = true;
+      return alexaService.getDeviceTypes()
+        .then(function(data) {
+          vm.alexaDevices = data.data
+          vm.ready = true;
+          vm.remoteIsBusy = false;
+          return
+        })
+        .catch(function(err) {
+          console.log(err)
+        })
     }
-  })();
+
+    function saveConfig() {
+      var alexaDevice = {};
+
+      vm.alexaDevices.forEach(function(room) {
+        room.deviceTypes.forEach(function(deviceType) {
+          if (deviceType.friendlyName) {
+            alexaDevice[deviceType.id] = { 'alexa': deviceType.alexa, 'friendlyName': deviceType.friendlyName }
+          }
+        });
+      });
+      return alexaService.saveConfig(alexaDevice)
+        .then(function(answer) {
+          if (answer.status == 200) {
+            if (answer.data === 'SAVE_SUCCESS') {
+              alexaService.successNotificationTranslated('CONFIG_SAVE');
+            } else {
+              alexaService.errorNotificationTranslated('CONFIG_UNSAVE')
+            }
+          } else {
+            alexaService.errorNotificationTranslated('CONFIG_UNSAVE')
+          }
+        })
+    }
+  }
+})();
